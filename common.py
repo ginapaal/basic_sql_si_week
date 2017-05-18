93f9f78b86387669a0e7f8557b22b7f68b0d4e92
Gina <paalginaa@gmail.com>
2017-05-18 11:15:37 +0200

    menu - user input added to common.py

--- common.py
+++ common.py
@@ -1,6 +1,17 @@
 import psycopg2
 
 
+def menu():
+    user_input = input("""1.) List all the mentors \n
+                        2.) Nicknames of mentors in Miskolc \n
+                        3.) Carol Smth left her hat here, what is her full name and phone number? \n
+                        4.) Find the girl by her e-mail \n
+                        5.) New applicant \n
+                        6.) Jemima Foreman's number has changed \n
+                        7.) Arsenio and friend want to cancel the process \n
+                        8.) Exit""")
+
+
 def make_connection():
     connect_str = "dbname = 'gina' user='gina' host='localhost' password ='thebest'"
     conn = psycopg2.connect(connect_str)
