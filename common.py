10a60320c8fb748e80b11b4892a7029b824ed9ae
Gina <paalginaa@gmail.com>
2017-05-18 11:58:47 +0200

    in common.py created a new function to handle cursor executions

--- common.py
+++ common.py
@@ -28,8 +28,13 @@ def print_query(query, conn):
     return print(rows)
 
 
+def cursor_execute(query, conn):
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+
 def list_mentor_names(conn):
-    print_query("""SELECT first_name, last_name FROM mentors;""", conn)
+    print_query("""SELECT first_name, last_name FROM mentors;""")
 
 
 def nicknames(conn):
@@ -49,12 +54,10 @@ def new_applicant(conn):
 
 
 def jemima_number_update(conn):
-    cursor = conn.cursor()
-    cursor.execute(
+    cursor_execute(
         """UPDATE applicants SET phone_number = '003670/223-7459' WHERE application_code = 58324;""", conn)
     print_query("""SELECT first_name, last_name, phone_number FROM applicants WHERE application_code = 58324;""", conn)
 
 
 def cancel_process(conn):
-    cursor = conn.cursor()
-    cursor.execute("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""")
+    cursor_execute("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""", conn)
