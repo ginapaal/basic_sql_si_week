4e8fc90c30537ef3a6d15420d54ab8d5fde16b51
Gina <paalginaa@gmail.com>
2017-05-18 11:52:06 +0200

    user_input handled in main.py. The program quits after every input, need to be fixed

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ from common import *
 import sys
 
 
-def main():
+def main(user_input):
     conn = make_connection()
-    list_mentor_names(conn)
-    nicknames(conn)
-    carol(conn)
-    other_girl(conn)
-    new_applicant(conn)
-    jemima_number_update(conn)
-    cancel_process(conn)
+    try:
+        if user_input == 1:
+            list_mentor_names(conn)
+        elif user_input == 2:
+            nicknames(conn)
+        elif user_input == 3:
+            carol(conn)
+        elif user_input == 4:
+            other_girl(conn)
+        elif user_input == 5:
+            new_applicant(conn)
+        elif user_input == 6:
+            jemima_number_update(conn)
+        elif user_input == 7:
+            cancel_process(conn)
+        elif user_input == 8:
+            sys.exit()
+    except ValueError:
+        print("You have to choose a number between 1-8. Try again!")
+        input(": ")
 
 
 if __name__ == "__main__":
-    main()
+    user_input = menu()
+    main(user_input)
