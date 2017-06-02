2e0b229ea40330e6e29cd685c7b1ec0858b8d718
Gina <paalginaa@gmail.com>
2017-06-02 12:41:43 +0200

    2.function - all_school- initialized with the query and the html template, in main_page.html i gave the route to the action

--- main.py
+++ main.py
@@ -18,5 +18,13 @@ def mentors_and_schools():
     return render_template('mentors.html', rows=rows)
 
 
+@app.route('/all-school', methods=['GET', 'POST'])
+def all_shool():
+    conn = common.make_connection()
+    rows = query.all_school(conn)
+    conn.close()
+    return render_template('all_school.html', rows=rows)
+
+
 if __name__ == "__main__":
     app.run()
--- query.py
+++ query.py
@@ -3,6 +3,12 @@ from common import *
 
 
 def mentors(conn):
-    rows = ask_query("""SELECT mentors.first_name, mentors.last_name, schools.name, schools.country FROM mentors
-                    LEFT JOIN schools ON mentors.city = schools.city;""", conn)
+    rows = ask_query("""SELECT mentors.id, mentors.first_name, mentors.last_name, schools.name, schools.country FROM mentors
+                    LEFT JOIN schools ON mentors.city = schools.city ORDER BY mentors.id ASC;""", conn)
+    return rows
+
+
+def all_school(conn):
+    rows = ask_query("""SELECT mentors.id, mentors.first_name, mentors.last_name, schools.name, schools.country FROM mentors
+                        RIGHT JOIN schools ON mentors.city = schools.city ORDER BY mentors.id ASC;""", conn)
     return rows
