dd78406295a5d9fa7df1350b5f8743b5b2bdc262
Gina <paalginaa@gmail.com>
2017-06-02 10:44:46 +0200

    main.py: console menu --> web menu; main_page.html: buttons added to navigate to the pages

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from common import *
 from query import *
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
-def main(user_input):
-    user_input = 8
-    while user_input > 0 and user_input <= 8:
-        user_input = menu()
-        conn = make_connection()
-        if user_input == 1:
-            list_mentor_names(conn)
-        elif user_input == 2:
-            nicknames(conn)
-        elif user_input == 3:
-            carol(conn)
-        elif user_input == 4:
-            other_girl(conn)
-        elif user_input == 5:
-            new_applicant(conn)
-        elif user_input == 6:
-            jemima_number_update(conn)
-        elif user_input == 7:
-            cancel_process(conn)
-        elif user_input == 0:
-            break
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return render_template('main_page.html')
+
+
+@app.route('/mentors', methods=['GET'])
+def mentors_and_schools():
+    pass
 
 
 if __name__ == "__main__":
-    user_input = menu()
-    main(user_input)
+    app.run()
